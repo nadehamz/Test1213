7e500149f5da9a6b8a98cc94f7300f3331a12117
hamzahnadeem16 <hamzah.nadeem@gmail.com>
2024-02-02 15:59:57 -0500

    1

---
+++ src/test1213/Test1213.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package test1213;
+
+/**
+ *
+ * @author User Hamzah Nadeem
+ */
+public class Test1213 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO 
+        
+    }
+
+}
